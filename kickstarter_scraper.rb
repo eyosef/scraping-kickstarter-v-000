d5ef9fe8dd715613f53cf5e3c39e374334f65593
Eden Mesfun <emesfun@gmail.com>
2018-02-19 17:25:19 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,8 +1,9 @@
 require 'nokogiri'
-html = File.read('fixtures/kickstarter.html')
-kickstarter = Nokogiri::HTML(html)
+
 
   def create_project_hash
+    html = File.read('fixtures/kickstarter.html')
+    kickstarter = Nokogiri::HTML(html)
 
     projects = {}
 
