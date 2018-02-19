4deb0ecf5d77837c34d4ea3670eedb60983ce80e
Eden Mesfun <emesfun@gmail.com>
2018-02-19 17:21:38 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -20,14 +20,3 @@ def create_project_hash
   projects
 
 end
-# projects: kickstarter.css("li.project.grid_4")
-# title: project.css("h2.bbcard_name strong a").text
-# image link: project.css("div.project-thumbnail a img").attribute("src").value
-# description: project.css("p.bbcard_blurb").text
-# project location: project.css("ul.project-meta span.location-name").text
-# percent funded: percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-
-
-create_project_hash
-
-
