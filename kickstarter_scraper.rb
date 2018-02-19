92443255aa429656aadf27bed1a16d7cfef11bfc
Eden Mesfun <emesfun@gmail.com>
2018-02-19 17:15:16 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,8 +8,10 @@ kickstarter = Nokogiri::HTML(html)
 def create_project_hash
   projects = {}
 
-  
-  
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end 
+
 end
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
